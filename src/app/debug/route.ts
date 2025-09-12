bd4e903f15847ebd139a31df73e28c51495c0699
paris <hwangseok.paris@gmail.com>
2025-09-12 14:46:24 +0900

    250912: 디버깅용 로그 제거

--- src/app/debug/route.ts
+++
@@ -1,7 +0,0 @@
-export const dynamic = "force-dynamic";
-
-export async function GET() {
-  return new Response(JSON.stringify({ now: Date.now() }), {
-    headers: { "content-type": "application/json" },
-  });
-}
